1e379fb14b334aa4e450c9e8e6f4c1a7fe4659e0
Chirayu <chirayupancholi17@gmail.com>
2023-08-17 20:46:21 +0530

    Completed easy problems

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,42 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let arr = {}
+
+  for (const c of str1) {
+    arr[c] = (arr[c] || 0) + 1;
+  }
+
+  for (const c of str2) {
+    if (!arr[c]) return false;
+    arr[c]--;
+  }
+
+  return Object.values(arr).every(cnt => cnt === 0)
 
 }
+
+const testCases = [
+  ['listen', 'silent', true],
+  ['hello', 'world', false],
+  ['abcdef', 'fedcba', true],
+  ['abc', 'cbaa', false],
+  ['anagram', 'nagaram', true],
+  ['12345', '54321', true],
+  ['abcd', 'dcbaa', false],
+  ['', '', true],
+];
+let passed = 0;
+
+for (const [str1, str2, expected] of testCases) {
+  const result = isAnagram(str1, str2);
+  if (result === expected) passed++;
+  console.log(`Are "${str1}" and "${str2}" anagrams? Expected: ${expected}, Result: ${result}`);
+}
+console.log('')
+console.log(`${passed} testcases passed out of ${testCases.length}`)
+console.log('')
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,42 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  let obj = {}
+
+  for (const transaction of transactions) {
+    obj[transaction.category] = (obj[transaction.category] || 0) + transaction.price;
+  }
+
+  let ans = [];
+
+  for (const category in obj) {
+    const categoryTotal = {};
+    categoryTotal[category] = obj[category];
+    ans.push(categoryTotal);
+  }
+
+  return ans;
 }
+
+const transactions = [
+  { itemName: 'Item 1', category: 'Groceries', price: 25, timestamp: 1631808000000 },
+  { itemName: 'Item 2', category: 'Electronics', price: 350, timestamp: 1631718000000 },
+  { itemName: 'Item 3', category: 'Groceries', price: 15, timestamp: 1631631600000 },
+  { itemName: 'Item 4', category: 'Clothing', price: 50, timestamp: 1631545200000 },
+  { itemName: 'Item 5', category: 'Electronics', price: 200, timestamp: 1631458800000 },
+  { itemName: 'Item 6', category: 'Clothing', price: 30, timestamp: 1631372400000 },
+  { itemName: 'Item 7', category: 'Groceries', price: 40, timestamp: 1631286000000 },
+];
+
+const expectedOutput = [
+  { Groceries: 80 },
+  { Electronics: 550 },
+  { Clothing: 80 },
+];
+
+const result = calculateTotalSpentByCategory(transactions);
+console.log(result)
+console.log(JSON.stringify(result) === JSON.stringify(expectedOutput));
 
 module.exports = calculateTotalSpentByCategory;
