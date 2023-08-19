7e41e676721dedb4b6215cb59895269301949eb5
Chirayu <chirayupancholi17@gmail.com>
2023-08-19 18:06:29 +0530

    01-js medium done

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,29 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  let n = str.length;
+
+  for (let i = 0; i < n + 1 / 2; i++) {
+    if (str[i] !== str[n - i - 1]) return false;
+  }
+
   return true;
 }
+
+const testCases = [
+  { input: 'racecar', expected: true },
+  { input: 'hello', expected: false },
+  { input: 'Anna', expected: true }, // Case-insensitive
+  { input: 'Nan', expected: true },  // Case-insensitive
+  { input: '12321', expected: true }, // Numeric palindrome
+];
+
+// Test the function
+testCases.forEach(testCase => {
+  const result = isPalindrome(testCase.input);
+  const status = result === testCase.expected ? 'PASSED' : 'FAILED';
+  console.log(`Input: ${testCase.input} | Expected: ${testCase.expected} | Result: ${result} | Status: ${status}`);
+});
 
 module.exports = isPalindrome;
